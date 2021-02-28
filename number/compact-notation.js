59b766d54259ed75d5741b43211e213ad01ef22a
Artem Gusev <gusev2014russia@mail.ru>
2021-02-28 10:08:28 +0300

    adds comments for compact-notation.js file

--- number/compact-notation.js
+++ number/compact-notation.js
@@ -3,9 +3,9 @@ const {NUMBER} = require('./index');
 console.log(`en-US short compact display: ${NUMBER.toLocaleString('en-US', {
   notation: 'compact',
   compactDisplay: 'short'
-})}`)
+})}`); //12K
 
 console.log(`ru-RU long compact display: ${NUMBER.toLocaleString('ru-RU', {
   notation: 'compact',
   compactDisplay: 'long'
-})}`);
+})}`); //12 тысяч
