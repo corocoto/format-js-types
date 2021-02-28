fecefa7fca48b45b7ae7fb842037f9a4f7bddf76
Artem Gusev <gusev2014russia@mail.ru>
2021-02-28 11:45:08 +0300

    Adds constants for working with date-time values

---
+++ date/index.js
@@ -0,0 +1,13 @@
+const CURRENT_DATE = new Date();
+
+const DATE_OPTIONS = {
+  weekday: 'long',
+  year: 'numeric',
+  month: 'long',
+  day: 'numeric'
+};
+
+module.exports = {
+  CURRENT_DATE,
+  DATE_OPTIONS
+};
